424ceb80bac99150e90ee34017c6cc58b296e641
johannkrugell <johannkrugell@gmail.com>
2023-03-08 06:18:44 +1100

    ADD INSERT AND DELETE METHODS Add methods to insert and delete nodes from tree. Also add tests for these methods. Also add a method to print the tree in order.
    On branch insert-delete
    Changes to be committed:
    	modified:   main.rb
    	modified:   spec/main-spec.rb

--- main.rb
+++ main.rb
@@ -34,4 +34,77 @@ class Tree < Node
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
+
+  def insert(value, current_node = @root)
+    if value < current_node.data
+      if current_node.left.nil?
+         current_node.left = Node.new(value)
+      else
+        insert(value, current_node.left)
+      end
+    elsif value > current_node.data
+      if current_node.right.nil?
+        current_node.right = Node.new(value)
+      else
+        insert(value, current_node.right)
+      end
+    end
+  end
+
+  def search(value, current_node = @root)
+    if current_node.nil?
+      return current_node
+    elsif value < current_node.data
+      search(value, current_node.left)
+    elsif value > current_node.data
+      search(value, current_node.right)
+    else
+      puts "Value is #{current_node.data}"
+      return current_node
+    end
+  end
+
+  # delete leaf from tree
+  def delete(value, current_node = @root)
+    if current_node.nil?
+      return current_node
+    elsif value < current_node.data
+      current_node.left = delete(value, current_node.left)
+    elsif value > current_node.data
+      current_node.right = delete(value, current_node.right)
+    else
+      # delete leaf
+      if current_node.left.nil? && current_node.right.nil?
+        current_node = nil
+      elsif
+      # delete node with one child
+        if current_node.left.nil?
+          temp = current_node
+          current_node = current_node.right
+          temp = nil
+        elsif current_node.right.nil?
+          temp = current_node
+          current_node = current_node.left
+          temp = nil
+        else
+          temp = find_min(current_node.right)
+          current_node.data = temp.data
+          current_node.right = delete(temp.data, current_node.right)
+        end
+      else
+        # delete node with two children
+        if !current_node.left.nil? && !current_node.right.nil?
+          temp = find_min(current_node.right)
+          current_node.data = temp.data
+          current_node.right = delete(temp.data, current_node.right)
+        end        
+      end
+    end
+    return current_node
+  end
+
+  def find_min(current_node)
+    current_node = current_node.left until current_node.left.nil?
+    current_node
+  end
 end
--- spec/main-spec.rb
+++ spec/main-spec.rb
@@ -19,19 +19,42 @@ end
 
 class TreeTest < Minitest::Test
   def setup
-    @tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
+    @tree = Tree.new([1, 8, 3, 4, 10, 6, 7, 11])
   end
 
   def test_initialize_tree
-    assert_equal 4, @tree.root.data
+    assert_equal 7, @tree.root.data
   end
 
   def test_build_tree
-    assert_equal 2, @tree.root.left.data
-    assert_equal 6, @tree.root.right.data
-    assert_equal 1, @tree.root.left.left.data
-    assert_equal 3, @tree.root.left.right.data
-    assert_equal 5, @tree.root.right.left.data
-    assert_equal 7, @tree.root.right.right.data
+    assert_equal 7, @tree.root.data
+    assert_equal 4, @tree.root.left.data
+    assert_equal 10, @tree.root.right.data
+    assert_equal 3, @tree.root.left.left.data
+    assert_equal 6, @tree.root.left.right.data
+    assert_equal 8, @tree.root.right.left.data
+    assert_equal 11, @tree.root.right.right.data
+    assert_equal 1, @tree.root.left.left.left.data
   end
+
+  def test_insert
+    @tree.insert(9)
+    assert_equal 9, @tree.root.right.left.right.data
+    @tree.insert(2)
+    assert_equal 2, @tree.root.left.left.left.right.data
+  end
+
+  def test_delete
+    # delete leaf
+    @tree.delete(10)
+    assert_nil @tree.root.right.right
+    # delete node with one child
+    @tree.delete(3)
+    assert_equal 1, @tree.root.left.left.data
+    # delete node with two children
+    @tree.delete(10)
+    assert_equal 11, @tree.root.right.data
+  end
+
+
 end
